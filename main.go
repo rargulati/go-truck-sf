32537a06b18853fd83683c77b166fb3f3ed7a0fe
Raj Gulati <rargulati@gmail.com>
2015-07-06 04:45:29 -0700

    Converged to correct json representation. Had to do with upper case field names for public membership.

--- main.go
+++ main.go
@@ -9,40 +9,37 @@ import (
 	"encoding/json"
 )
 
-var APIKey string
-/*
-type location struct {
+type Location struct {
 	needs_recoding	bool		`json:"needs_recoding"`
 	longitude				string	`json:"longitude"`
 	latitude				string	`json:"latitude"`
 }
 
-type mobileFood struct {
-	location						json.RawMessage
-	status							string	`json:"status,omitempty"`
-	expirationdate			string	`json:"expirationdate,omitempty"`
-	permit							string	`json:"permit,omitempty"`
-	block								string	`json:"block,omitempty"`
-	received						string	`json:"received,omitempty"`
-	facilitytype				string	`json:"facilitytype,omitempty"`
-	blocklot						string	`json:"blocklot,omitempty"`
-	noisent							string	`json:"noisent,omitempty"`
-	locationdescription	string	`json:"locationdescription,omitempty"`
-	cnn									string	`json:"cnn,omitempty"`
-	priorpermit					string	`json:"priorpermit,omitempty"`
-	approved						string	`json:"approved,omitempty"`
-	schedule						string	`json:"schedule,omitempty"`
-	address							string	`json:"address,omitempty"`
-	applicant						string	`json:"applicant,omitempty"`
-	lot									string	`json:"lot,omitempty"`
-	fooditems						string	`json:"fooditems,omitempty"`
-	longitude						string	`json:"longitude,omitempty"`
-	latitude						string	`json:"latitude,omitempty"`
-	objectid						string	`json:"objectid,omitempty"`
-	y										string	`json:"y,omitempty"`
-	x										string	`json:"x,omitempty"`
+type Facility struct {
+	Location						*Location
+	Status							string	`json:"status,omitempty"`
+	Expirationdate			string	`json:"expirationdate,omitempty"`
+	Permit							string	`json:"permit,omitempty"`
+	Block								string	`json:"block,omitempty"`
+	Received						string	`json:"received,omitempty"`
+	Facilitytype				string	`json:"facilitytype,omitempty"`
+	Blocklot						string	`json:"blocklot,omitempty"`
+	Noisent							string	`json:"noisent,omitempty"`
+	Locationdescription	string	`json:"locationdescription,omitempty"`
+	Cnn									string	`json:"cnn,omitempty"`
+	Priorpermit					string	`json:"priorpermit,omitempty"`
+	Approved						string	`json:"approved,omitempty"`
+	Schedule						string	`json:"schedule,omitempty"`
+	Address							string	`json:"address,omitempty"`
+	Applicant						string	`json:"applicant,omitempty"`
+	Lot									string	`json:"lot,omitempty"`
+	Fooditems						string	`json:"fooditems,omitempty"`
+	Longitude						string	`json:"longitude,omitempty"`
+	Latitude						string	`json:"latitude,omitempty"`
+	Objectid						string	`json:"objectid,omitempty"`
+	Y										string	`json:"y,omitempty"`
+	X										string	`json:"x,omitempty"`
 }
-*/
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -61,37 +58,14 @@ func ShowResponse(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(nil, err.Error(), http.StatusInternalServerError)
 	}
-	var data interface{}
+	data := make([]Facility, 0)
 	defer res.Body.Close()
-	// w.Header().Set("Content_type", "application/json")
+
 	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		fmt.Println(err)
 		log.Fatal(err)
 		return
 	}
-	m := data.([]interface{})
-	for _,info := range m {
-		r := info.(map[string]interface{})
-		for k,v := range r {
-			switch vv := v.(type) {
-			case string:
-				fmt.Println(k, "is string", vv)
-			case int:
-				fmt.Println(k, "is int", vv)
-			case []interface{}:
-				fmt.Println(k, "is an array:")
-				for i, u := range vv {
-					fmt.Println(i, u)
-				}
-			case map[string]interface{}:
-				fmt.Println(k, "is nested json:")
-				for i, u := range vv {
-					fmt.Println(i, u)
-				}
-			default:
-				fmt.Println(k, "is of a type I don't know how to handle", v)
-			}
-		}
-	}
+	fmt.Println(&data)
 }
